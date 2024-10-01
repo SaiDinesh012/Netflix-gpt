2750d70af05335090eefc492a05a04af0138fcb0
saidinesh <sunnybolloju@gmail.com>
2024-10-01 14:13:40 +0530

    firrs commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-3xl text-green-800'>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
